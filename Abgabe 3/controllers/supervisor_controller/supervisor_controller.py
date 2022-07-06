1132047b6966931ea326be46ae4d71dcd22c15ce
tranngocvyvy <vyvy.tns@gmail.com>
2022-07-06 22:55:04 +0200

    supervisor can read positions

--- Abgabe 3/controllers/supervisor_controller/supervisor_controller.py
+++ Abgabe 3/controllers/supervisor_controller/supervisor_controller.py
@@ -1,52 +1,38 @@
-"""robo_controller controller."""
-
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
 from controller import Motor, Supervisor, Node
 from Generation import Generation, Genotype
 # create the Robot instance.
-#robot = Robot()
+
+#Generation = Generation(Genotype(40))
 supervisor = Supervisor()
+timestep = int(supervisor.getBasicTimeStep())
+#emitter = supervisor.getEmitter("emitter")
 robot= supervisor.getFromDef("Robot")
 trans_field=robot.getField("translation")
-# get the time step of the current world.
-timestep = int(10)
+rot_field = robot.getField("rotation")
+start_rot = rot_field.getSFRotation()
+start_pos=robot.getPosition()
+#start_rot=robot.getRotation()
 
-"""    
-maxMotorVelocity = 6
-hingejoints = []
-hgnames = ['shoulderleft']
-for name in hgnames:
-    hingejoints.append(robot.getDevice(name))
-speed = 2  # [rad/s]
-for idx,name in enumerate(hingejoints):
-    hingejoints[idx].setPosition(float('inf'))
-    hingejoints[idx].setVelocity(speed)    
-sensors=[]    
-sensor_names=['dsfrontmiddle','dsfrontright','dsfrontleft','dsleft','dsright','dsback']
-for s in sensor_names:
-    sensors.append(robot.getDevice(s))
+def fitness_function():
+    fitness=0
+    cur_pos=robot.getPosition()
+
+def reset_robot():
+    trans_field.setSFVec3f(start_pos)
+    rot_field.setSFRotation(start_rot)
+    trans_field.setSFVec3f(start_pos)
+    rot_field.setSFRotation(start_rot)
+def open_file(file):
+
+def start_loop():
+            
+    i=0
+    while supervisor.step(timestep) != -1:
+        i+=1
+        print('POSITION',robot.getPosition())
     
-for s in sensors:
-    print(s)
-"""
+       
+       
+        pass
     
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-i=0
-while supervisor.step(timestep) != -1:
-    i+=1
-    print('POSITION',robot.getPosition())
-
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-   
-    pass
-
 # Enter here exit cleanup code.
--- Abgabe 3/controllers/walking/walking.py
+++ Abgabe 3/controllers/walking/walking.py
@@ -1,42 +1,41 @@
-"""robo_controller controller."""
-
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
-from controller import Robot, Motor, Supervisor
+from controller import Robot, Motor, Supervisor, Receiver
+#import NN
 
 # create the Robot instance.
 robot = Robot()
+
 # get the time step of the current world.
-timestep = int(10)
-    
+timestep =  int(robot.getBasicTimeStep())
+
 maxMotorVelocity = 6
+speed = 2  # [rad/s]
+
 hingejoints = []
-hgnames = ['shoulderleft']
+hgnames = ['shoulderleft', 'shoulderright']
 for name in hgnames:
     hingejoints.append(robot.getDevice(name))
-speed = 2  # [rad/s]
+
 for idx,name in enumerate(hingejoints):
     hingejoints[idx].setPosition(float('inf'))
     hingejoints[idx].setVelocity(speed)    
+    
 sensors=[]    
 sensor_names=['dsfrontmiddle','dsfrontright','dsfrontleft','dsleft','dsright','dsback']
 for s in sensor_names:
     sensors.append(robot.getDevice(s))
 
+receiver = robot.getReceiver("receiver")
+receiver.enable(timestep)
     
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
+
 while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-   
- 
+    """ receive Data from emitter
+    if receiver.getQueueLength() > 0:
+        data = receiver.getData()
+        receiver.nextPacket()
+        weights = np.array(json.loads(data))
+        NN.setWeights(weights)
+    """
     pass
 
 # Enter here exit cleanup code.
