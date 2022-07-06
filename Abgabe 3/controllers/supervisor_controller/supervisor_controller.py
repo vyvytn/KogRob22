4754cb76ce0ac26775e20fab0ac185ae5f094209
tranngocvyvy <vyvy.tns@gmail.com>
2022-07-06 20:58:01 +0200

    added working supervisor to webots project

--- Abgabe 3/controllers/supervisor_controller/supervisor_controller.py
+++ Abgabe 3/controllers/supervisor_controller/supervisor_controller.py
@@ -2,13 +2,17 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Robot, Motor, Supervisor
-
+from controller import Motor, Supervisor, Node
+from Generation import Generation, Genotype
 # create the Robot instance.
-robot = Robot()
+#robot = Robot()
+supervisor = Supervisor()
+robot= supervisor.getFromDef("Robot")
+trans_field=robot.getField("translation")
 # get the time step of the current world.
 timestep = int(10)
-    
+
+"""    
 maxMotorVelocity = 6
 hingejoints = []
 hgnames = ['shoulderleft']
@@ -22,11 +26,18 @@ sensors=[]
 sensor_names=['dsfrontmiddle','dsfrontright','dsfrontleft','dsleft','dsright','dsback']
 for s in sensor_names:
     sensors.append(robot.getDevice(s))
-
+    
+for s in sensors:
+    print(s)
+"""
     
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
+i=0
+while supervisor.step(timestep) != -1:
+    i+=1
+    print('POSITION',robot.getPosition())
+
     # Read the sensors:
     # Enter here functions to read sensor data, like:
     #  val = ds.getValue()
@@ -36,7 +47,6 @@ while robot.step(timestep) != -1:
     # Enter here functions to send actuator commands, like:
     #  motor.setPosition(10.0)
    
- 
     pass
 
 # Enter here exit cleanup code.
