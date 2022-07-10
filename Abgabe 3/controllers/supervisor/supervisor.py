9d4df580958ac3d392340c861262c4e86aa26e6e
Nastasja Parschew <nastasja.parschew@outlook.com>
2022-07-10 19:04:20 +0200

    moved neural_network to supervisor package

--- Abgabe 3/controllers/supervisor/supervisor.py
+++ Abgabe 3/controllers/supervisor/supervisor.py
@@ -1,5 +1,5 @@
 from controller import Motor, Supervisor, Node, Emitter, Receiver
-#from robot.neural_network import NeuralNetwork
+from neural_network import NeuralNetwork
 from population import Population
 import json
 
@@ -16,20 +16,20 @@ robot = supervisor.getFromDef("Robot")
 trans_field = robot.getField("translation")
 rotation_field = robot.getField("rotation")
 
-param_right1= supervisor.getFromDef("right_arm1")
-axis_right1= param_right1.getField("axis")
-param_right2= supervisor.getFromDef("right_arm2")
-axis_right2= param_right2.getField("axis")
-position_right1= param_right1.getField("position")
-position_right2= param_right2.getField("position")
+param_right1 = supervisor.getFromDef("right_arm1")
+axis_right1 = param_right1.getField("axis")
+param_right2 = supervisor.getFromDef("right_arm2")
+axis_right2 = param_right2.getField("axis")
+position_right1 = param_right1.getField("position")
+position_right2 = param_right2.getField("position")
 
 
 param_left1 = supervisor.getFromDef("left_arm1")
-axis_left1= param_left1.getField("axis")
+axis_left1 = param_left1.getField("axis")
 param_left2 = supervisor.getFromDef("left_arm2")
-axis_left2= param_left2.getField("axis")
-position_left1= param_left1.getField("position")
-position_left2= param_left2.getField("position")
+axis_left2 = param_left2.getField("axis")
+position_left1 = param_left1.getField("position")
+position_left2 = param_left2.getField("position")
 #axis_right.setSFVec3f([0.0,0.0,1.0])
 #axis_right.getSFVec3f()
 
